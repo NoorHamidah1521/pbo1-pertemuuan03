c05ab8caf59679ad7c95784d620b37e80ed97bb9
noorhamidah <noorhamidah1521@gmail.com>
2020-05-02 06:30:22 -0700

    Percabangan If Else

---
+++ src/operator/IfElse.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class IfElse {
+public static void main (String[] args){
+    double totalBelanja = 53000;
+    double uangDidompet = 25000;
+
+    if (uangDidompet<totalBelanja){
+        System.out.println("Uang kurang , kurangi jajan anda");
+    }else if (uangDidompet>totalBelanja){
+        double angsul = uangDidompet-totalBelanja;
+        System.out.println("uang cukup , angsul : "+angsul);
+    }else{
+        System.out.println("Uang pas .. UwU..");
+    }
+}
+}
