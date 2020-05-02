9caea47cf8d32c8e0ad701aa5b148b4118b53dd8
noorhamidah <noorhamidah1521@gmail.com>
2020-05-02 06:12:44 -0700

    Operator Equality dan Relational

--- src/operator/OperatorEquality.java
+++ src/operator/OperatorEquality.java
@@ -7,7 +7,7 @@ public class OperatorEquality {
         System.out.println(i==5); //true
         System.out.println(i==6); //false
         System.out.println(i==j); //false
-        System.out.println(i>j);  //false
+        System.out.println(i>j);  //fals
         System.out.println(i<j); //true
         System.out.println(5>6); //false
         System.out.println(5<6); //true
